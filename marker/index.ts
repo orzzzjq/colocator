196286bbec7f3ea43a2f82625780d1e7891b2c7e
orzzzjq <orzzzjq@gmail.com>
2023-10-04 01:02:03 +0800

    try marker sample

--- marker/index.ts
+++ marker/index.ts
@@ -11,7 +11,7 @@ async function initMap() {
 
     const map = new Map(document.getElementById('map') as HTMLElement, {
         center: { lat: 37.4239163, lng: -122.0947209 },
-        zoom: 15,
+        zoom: 14,
         mapId: '4504f8b37365c3d0',
     });
 
